3431cdd47683697b9e8c13cc3a191da5d7d935f4
Liji Jinaraj <liji@vunite.com>
2015-01-25 22:54:28 -0800

    Trend initialization and generation

--- test/index-test.js
+++ test/index-test.js
@@ -1,16 +1,135 @@
 var vows = require('vows'),
-    assert = require('assert');
+    assert = require('assert'),
+    moment = require('moment');
 
 var suite = vows.describe("trendo");
 
 var trendo = require("../trendo");
 
 suite.addBatch({
-  "trendo": {
+  "trendo.version": {
   	topic: trendo.version,
+
     "is defined": function(version) {
       assert.isString(version);
     }
+  },
+
+  "trendo.trends": {
+	  	topic: trendo,
+
+	    "when no data is provided, returns undefined": function(trendo) {
+	      assert.isUndefined(trendo.trends());
+	    },
+
+	    "when data is not an array, returns undefined": function(trendo) {
+	      assert.isUndefined(trendo.trends("a,b,c"));
+	    },
+
+	    "calling `trendo.trends([])`": {
+	    	topic: function(trendo) {
+	    		return trendo.trends([]);
+	    	},
+
+	    	"should return default trends object": function(trends) {
+	    		assert.isObject(trends);
+	    		["total", "day", "week", "month", "year"].forEach(function(d) {
+
+	    			assert.include(trends, d);
+
+	    			var group = trends[d];
+	    			
+	    			assert.isObject(group);
+
+	    			assert.include(group, "unit");
+
+	    			if(d !== "total") {
+	    				assert.include(group, "length");
+	    				assert.include(group, "counts");
+	    				assert.isArray(group.counts)
+	    				assert.equal(group.length, group.counts.length);
+	    			}
+
+	    			assert.include(group, "timeframe");
+	    			assert.include(group.timeframe, "start");
+	    			assert.include(group.timeframe, "end");
+
+	    		});
+	    	},
+
+	    	"should return formatted hours in hourly trend data": function(trends) {
+	    		trends.day.counts.forEach(function(trend, i) {
+	    			var date = moment(new Date(trend.date));
+	    			assert.equal(trend.text, 
+	    				date.format(trends.day.unitFormat));
+	    		});
+	    	},
+
+	    	"should return formatted days in weekly trend data": function(trends) {
+	    		trends.week.counts.forEach(function(trend, i) {
+	    			var date = moment(new Date(trend.date));
+	    			assert.equal(trend.text, 
+	    				date.format(trends.week.unitFormat(date, i)));
+	    			
+	    			if(i === 0) 
+	    				assert.equal(trend.text, "Today");
+	    			else
+	    				assert.equal(/^(Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday)$/gi.test(trend.text), true);
+	    		});
+	    	},
+
+	    	"should return formatted weeks in montly trend data": function(trends) {
+	    		trends.month.counts.forEach(function(trend, i) {
+	    			var date = moment(new Date(trend.date));
+	    			assert.equal(trend.text, 
+	    				date.format(trends.month.unitFormat(date, i)));
+
+	    			if(i === 0) assert.equal(trend.text, "This Week");
+	    			if(i === 1) assert.equal(trend.text, "Last Week");
+	    			if(i > 1) assert.equal(/weeks ago$/gi.test(trend.text), true);
+	    		});
+	    	},
+
+	    	"should return formatted months in yearly trend data": function(trends) {
+	    		trends.year.counts.forEach(function(trend, i) {
+	    			var date = moment(new Date(trend.date));
+	    			assert.equal(trend.text, 
+	    				date.format(trends.year.unitFormat(date, i)));
+
+	    			if(i === 0) assert.equal(trend.text, "This Month");
+	    			if(i > 1) assert.equal(/^jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec/gi.test(trend.text), true);
+	    		});
+	    	}
+	    },
+
+	    "calling `trendo.trends([data,...])`": {
+	    	topic: function(trendo) {
+	    		var data = [];
+	    		var mDate = moment();
+	    		for(var i = 0; i < 24; i++) {
+	    			data.push({
+	    				date: mDate.clone().toDate().getTime(),
+	    				title: "d" + i
+	    			});
+	    			console.log(data[i].date);
+	    			mDate.subtract(1, 'hour');
+	    		}
+	    		return {data: data, trends: trendo.trends(data)};
+	    	},
+
+	    	"should return last day trends in hours": function(d) {
+	    		d.trends.day.counts.forEach(function(c, i) {
+	    			console.log(i + ": " + c)
+	    			assert.equal(c.count, 1);
+
+	    			var mDate = moment(d.data[i].date)
+	    				.set('minute', 0)
+	    				.set('second', 0)
+	    				.set('millisecond', 0);
+	    			assert.equal(c.date, mDate.toDate().getTime());
+	    		});
+	    	}
+	    }
   }
 });
 
--- trendo.js
+++ trendo.js
@@ -1,17 +1,134 @@
+var moment = require('moment');
+
 !function() {
 	var trendo = {
     version: "1.0.0"
   };
 
+  var TREND_GROUPS = ["day", "week", "month", "year"];
+
   trendo.trends = function(data) {
   	if(!(data instanceof Array)) return;
 
-  	return {
-  		pastDay: [],
-  		pastWeek: [],
-  		pastMonth: [],
-  		pastYear: []
+  	var response = {
+  		total: {
+  			unit: "total",
+  			timeframe: {start: 0, end: Date.now()},
+  			valid: {count: 0, data: []},
+  			invalid: {count: 0, data: []},
+  			excluded: {count: 0, data: []}
+  		}  	
   	};
+
+  	var mNow = moment(response.total.timeframe.end);
+
+  	// Init trend groups
+  	response["day"] = createTrendGroup('hour', 24, mNow, 'h:00a');
+  	response["week"] = createTrendGroup('day', 7, mNow, function(mDate, i) {
+  		return (i === 0) ? '[Today]' : 'dddd';
+  	});
+  	response["month"] = createTrendGroup('week', 5, mNow, function(mDate, i) {
+  		return "[" 
+  			+ ((i === 0) 
+  				? 'This Week' : (i === 1)
+  				? 'Last Week' : i + " weeks ago")
+  			+ "]";
+  	});
+  	response["year"] = createTrendGroup('month', 12, mNow, function(mDate, i) {
+  		return (i === 0) ? '[This Month]' : "MMM 'YY";
+  	});
+
+  	response.total.timeframe.start = moment(response.year.timeframe.start)
+  		.startOf('day')
+  		.toDate().getTime();
+
+  	data.forEach(function(d, i) {
+  		if(isInvalidDate(d.date)) {
+				response.total.invalid.data.push(d);
+  			response.total.invalid.count++;
+  			return;
+  		}
+
+  		if(shouldExcludeDate(d.date, response.total.timeframe)) {
+  			response.total.excluded.data.push(d);
+  			response.total.excluded.count++;
+  			return;
+  		}
+
+  		TREND_GROUPS.forEach(function(key) {
+  			var trend = response[key],
+  					mDate,
+  					offset,
+  					index;
+
+  			mDate = moment(d.date)
+  				.set('minute', 0)
+  				.set('second', 0)
+  				.set('millisecond', 0);
+
+  			if(d.unit !== 'hour') mDate.set('hour', 0);
+
+  			if(shouldExcludeDate(mDate.toDate().getTime(), trend.timeframe)) return;
+
+  			switch(key) {
+  				case 'day':
+  					var hour = mDate.hour();
+  					offset = moment(response.day.counts[0].date).hour();
+  					index = (hour <= offset) ? offset - hour : offset + (hour - offset);
+  					response.day.counts[index].count++;
+  					response.day.counts[index].data.push(d);
+  					break;
+  				// case 'week':
+  				// 	var day = mDate.day();
+  				// 	offset = moment(response.week.counts[0].date).day();
+  				// 	index = (day <= offset) ? offset - day : offset + (day - offset);
+  				// 	response.week.counts[index].count++;
+  				// 	response.week.counts[index].data.push(d);
+  				// 	break;
+  			}
+  		});
+  	});
+  	
+  	return response;
+  };
+
+  function createTrendGroup(unit, length, mDate, format) {
+		var trend = {
+			unit: unit,
+			unitFormat: format,
+			length: length,
+			timeframe: {start: 0, end: 0},
+			counts: []
+		};
+
+		var mNow = mDate.clone()
+  		.set('minute', 0)
+  		.set('second', 0)
+  		.set('millisecond', 0);
+
+  	if(unit !== 'hour') mNow.set('hour', 0);
+  		
+  	trend.timeframe.end = mNow.toDate().getTime();
+  	for(var d = 0; d < trend.length; d++) {
+  		mNow = mNow.clone().subtract(d > 0 ? 1 : 0, unit);
+  		trend.counts[d] = {
+  			text: mNow.format(typeof format === 'function' ? format(mNow, d) : format), 
+  			date: mNow.toDate().getTime(), 
+  			count: 0, 
+  			data: []
+  		};
+  	}
+  	trend.timeframe.start = mNow.toDate().getTime();
+
+  	return trend;
+  }
+
+  function isInvalidDate(date) {
+  	return isNaN(new Date(date));
+  }
+
+  function shouldExcludeDate(date, timeframe) {
+  	return date < timeframe.start || date > timeframe.end;
   }
 
   if (typeof define === "function" && define.amd) 
